0f7c1914c82cd78584c1c89021219cb54ac93639
Martino Mensio <martinomensio@outlook.it>
2017-06-09 19:25:55 +0200

    explaining a bit better the soft and hard constraints this refers #1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -84,46 +84,76 @@ public class Main {
 			// Create the instace and solve the problem
 
 			// SOFT CONSTRAINT
+			/*
+			The soft constraint adds penalties for solutions that we don't like.
+			It is used as a cost calculator instead of the default one.
+			The iterations will select the solution with lower cost, so applying penalties will make those solutions less attractive.
+			*/
 			final StateManager stateManager = new StateManager(vrp);
 			ConstraintManager constraintManager = new ConstraintManager(vrp, stateManager);
 
 			SolutionCostCalculator costCalculator = new SolutionCostCalculator() {
 				@Override
 				public double getCosts(VehicleRoutingProblemSolution solution) {
+					// initialize the cost to 0
 					double costs = 0.;
+					// for each route compute the cost (standard cost)
 					for (VehicleRoute route : solution.getRoutes()) {
+						// add the cost of the vehicle
 						costs += route.getVehicle().getType().getVehicleCostParams().fix;
+						// add the cost of the route
 						costs += stateManager.getRouteState(route, InternalStates.COSTS, Double.class);
 					}
 					int nRoutes = solution.getRoutes().size();
 					// penalty for unused routes
+					// the penalty is proportional to how much the used number of vehicle is distant from the desired
+					// the multiplication costant is selected to make this a big penalty
 					costs += (MyUtil.numVehiclesToUse - nRoutes) * 1000000;
 					// penalty for unassigned jobs
 					costs += solution.getUnassignedJobs().size() * 1000000;
+					// among all the routes, find the minimum number of jobs for a single route
 					int minJobsInRoute = solution.getRoutes().stream().mapToInt(getRouteActivitiesSize).min().getAsInt();
+					// among all the routes, find the maximum number of jobs for a single route
 					int maxJobsInRoute = solution.getRoutes().stream().mapToInt(getRouteActivitiesSize).max().getAsInt();
+					// calculate the ratio between the two found values (this is a measure of unbalancedness)
 					double jobsRate = ((double)maxJobsInRoute) / minJobsInRoute;
 					// penalty for unbalanced routes
 					if (MyUtil.wantBalanced && jobsRate > MyUtil.balanceFactor) {
+						// if this ratio is too high, apply penalty
+						// this penalty is smaller than the one applied for wrong number of vehicles or for unassigned jobs
+						// because the balance is a less important factor
 						costs += 100000 * jobsRate;
 					}
 					return costs;
 				}
 			};
 			// HARD constraint in insertion
+			/*
+			This is applied in a insertionContext, building the solution. It does not allow some solutions to be built
+			*/
 			HardRouteConstraint routeLevelConstraint = new HardRouteConstraint() {
+				// limits for the number of jobs per tour.
+				// Both are near the value MyUtil.nJobs/ (MyUtil.numVehiclesToUse - 1) that could allow an equal division of the jobs without using a vehicle.
+				// if few vehicles, the constraint is the floor of this value because we don't want to pass over this value
 				private int maxJobPerTourLowN = (int)Math.floor(((double)MyUtil.nJobs) / (MyUtil.numVehiclesToUse - 1));
+				// if more vehicles, we should give a higher margin of freedom, because if more vehicles are used, less jobs are served by each one of them
 				private int maxJobPerTourHighN = (int)Math.ceil(((double)MyUtil.nJobs) / (MyUtil.numVehiclesToUse - 1)) + 1;
-				
+				// those limits do not provide a general solution to the problem. They have been found by doing some experiments and could be defined better.
+
+
+				// This function is evaluated in order to understand if an insertion can be done on a certain route
 				@Override
 				public boolean fulfilled(JobInsertionContext insertionContext) {
 					int maxJobPerTour;
+					// apply our naif criteria
 					if (MyUtil.numVehiclesToUse > 9) {
 						maxJobPerTour = maxJobPerTourHighN;
 					} else {
 						maxJobPerTour = maxJobPerTourLowN;
 					}
+					// if our criteria is not satisfied (already reached the limit on number of jobs for this tour)
 					if (insertionContext.getRoute().getTourActivities().getActivities().size() >= maxJobPerTour) {
+						// forbid the insertion
 						return false;
 					}
 					return true;
@@ -134,11 +164,14 @@ public class Main {
 			// START
 			VehicleRoutingAlgorithmBuilder vraBuilder = new VehicleRoutingAlgorithmBuilder(vrp,
 					(String) options.get(CONSTANTS.CONFIG));
+			// these two lines add the hard constraint
 			constraintManager.addConstraint(routeLevelConstraint);
 			vraBuilder.setStateAndConstraintManager(stateManager, constraintManager);
+
 			vraBuilder.addCoreConstraints();
 			vraBuilder.addDefaultCostCalculators();
 			vraBuilder.setNuOfThreads((int) options.get(CONSTANTS.THREADS));
+			// this line subtitutes the default cost calculator, linking the soft constraint
 			vraBuilder.setObjectiveFunction(costCalculator);
 			VehicleRoutingAlgorithm vra = vraBuilder.build();
 			// END
